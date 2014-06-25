c0acd9a0e8b857b161101d85a1fdd8d1302e3f05
Lee Miller <iarkeus@gmail.com>
2014-06-25 10:05:52 -0700

    Upgrades.

--- app/assets/javascripts/data/upgrades_database.js
+++ app/assets/javascripts/data/upgrades_database.js
@@ -1,11 +1,14 @@
 var UpgradesDatabase = function() {
 	this.data = [
+		/**
+		 * name: name of the upgrade
+		 * rpA: additive amount of RP per second
+		 * rpM: multiplicative amount of RP per second
+		 * cpA: additive amount of CC per second
+		 * cpM: multiplicative amount of CC per second 
+		 */
 		{
-			name: "Research Focus",
-			rpA: 1,
-			rpM: 0,
-			ccA: 0,
-			ccM: 0,
+			name: "Research Focus", rpA: 1, rpM: 0, ccA: 0, ccM: 0,
 		}
 	];
 };
