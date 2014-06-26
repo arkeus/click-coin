7e8d81515d41b933160357cb49263daa74a747a1
Lee Miller <iarkeus@gmail.com>
2014-06-26 01:02:26 -0700

    Upgrades.

--- app/assets/javascripts/data/upgrades_database.js
+++ app/assets/javascripts/data/upgrades_database.js
@@ -7,8 +7,10 @@ var UpgradesDatabase = function() {
 		 * cpA: additive amount of CC per second
 		 * cpM: multiplicative amount of CC per second 
 		 */
-		{
-			name: "Research Focus", rpA: 1, rpM: 0, ccA: 0, ccM: 0,
-		}
+		{ id: "rf1", name: "Research Focus I", rpA: 1, rpM: 0, ccA: 0, ccM: 0, req: [] },
+		{ id: "rf2", name: "Research Focus II", rpA: 1, rpM: 0, ccA: 0, ccM: 0, req: ["rf1"] },
+		{ id: "rf3", name: "Research Focus III", rpA: 1, rpM: 0, ccA: 0, ccM: 0, req: ["rf2"] },
+		{ id: "rf4", name: "Research Focus IV", rpA: 1, rpM: 0, ccA: 0, ccM: 0, req: ["rf3"] },
+		{ id: "rf5", name: "Research Focus V", rpA: 1, rpM: 0, ccA: 0, ccM: 0, req: ["rf4"] },
 	];
 };
