e1b16c5891c81b95ad33c646644a90cffa768c91
Lee Miller <iarkeus@gmail.com>
2014-06-29 03:42:19 -0700

    Cpu

---
+++ app/assets/javascripts/data/cpu_database.js
@@ -0,0 +1,11 @@
+var CPUDatabase = function() {
+	this.data = [
+		/** Something. */
+		{ name: "Outel Celery 100", cpA: 0.1 },
+		{ name: "Outel Celery 200", cpA: 0.2 },
+		{ name: "Outel Celery 300", cpA: 0.4, req: ["cpu1"] },
+		{ name: "Outel Celery 301", cpA: 0.8 },
+		{ name: "Outel Celery x105", cpA: 1.5 },
+		{ name: "Outel Celery x267", cpA: 4.0, req: ["cpu2"] },
+	];
+};
