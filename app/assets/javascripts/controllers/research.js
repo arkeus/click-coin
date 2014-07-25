744baa6b934de58476a842c22ce4817fc835f93d
Lee Miller <iarkeus@gmail.com>
2014-07-24 21:36:11 -0700

    Tick.

--- app/assets/javascripts/controllers/research.js
+++ app/assets/javascripts/controllers/research.js
@@ -1,6 +1,7 @@
 app.controller("ResearchController", ["$scope", "Data", "Upgrades", function($scope, Data, Upgrades) {
 	$scope.upgrades = Upgrades.data;
 	$scope.researchers = Data.researchers;
+	$scope.rp = Data.rp;
 	
 	$scope.isUnlocked = function() {
 		return Data.company.obtained;
@@ -17,5 +18,9 @@ app.controller("ResearchController", ["$scope", "Data", "Upgrades", function($sc
 		Data.research(upgrade.id);
 		// TODO: add ability to add messages to log
 		// TODO: research points
+	};
+	
+	$scope.tick = function() {
+		$scope.rp.value += 1;
 	};
 }]);
